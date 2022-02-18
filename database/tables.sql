58719718e973834961568768d3f303f4ca22b150
covenroven <harka.dnl@gmail.com>
2022-02-18 16:30:21 +0700

    Fix tables.sql

--- database/tables.sql
+++ database/tables.sql
@@ -2,7 +2,7 @@ create table orders (
 	order_id SERIAL primary key,
 	customer_name varchar(50) not null,
 	ordered_at timestamp not null
-)
+);
 
 create table items (
 	item_id serial primary key,
@@ -11,4 +11,4 @@ create table items (
 	quantity int not null,
 	order_id int not null,
 	constraint fk_order_id foreign key(order_id) references orders(order_id)
-)
+);
